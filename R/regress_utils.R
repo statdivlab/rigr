ef6dfcaaa7a48b9679d79bd6cca0f7b6a45da4b2
Charles Wolock <cwolock@gmail.com>
2021-08-09 16:09:35 -0700

    more regress_utils organizing

--- R/regress_utils.R
+++ R/regress_utils.R
@@ -1872,3 +1872,72 @@ print.augCoefficients <-
     invisible(frmtCoefficients)
   }
 
+fitted.uRegress <-
+  function (object,...,X, version=FALSE) {
+    
+    obj <- object
+    vrsn <- "20110928"
+    if (version) return(vrsn)
+    
+    
+    if (!missing(X)) {
+      if (dim(X)[2] != dim(obj$processTerms$X)[2]) stop("new data not appropriate dimensions")
+      if (dim(obj$coefficients)[1] == dim(X)[2] + 1) X <- cbind(1,X)
+      fits <- X %*% obj$coefficients[,1]
+    } else fits <- obj$glmobj$fitted.values
+    if (obj$functional == "geometric mean") fits <- exp(fits)
+    fits
+  }
+
+#' Prediction Intervals for \code{uRegress} objects
+#' 
+#' Produces prediction intervals for objects of class \code{uRegress}.
+#' 
+#' 
+#' @aliases predict.uRegress predict
+#' @param object an object of class \code{uRegress}.
+#' @param interval Type of interval calculation
+#' @param level Tolerance/confidence level
+#' @param ...  other arguments to pass to the appropriate predict function for
+#' the class of \code{object$fit}. See \code{\link[survival]{predict.coxph}},
+#' \code{\link[stats]{predict.lm}}, or \code{\link[stats]{predict.glm}} for
+#' more details. Predictions are not currently implemented for objects of type
+#' \code{\link[geepack]{geeglm}}.
+#' @return %% ~Describe the value returned Returns a matrix with the fitted
+#' value and prediction interval for the entered X. 
+#' @seealso \code{\link[uwIntroStats]{regress}}
+#' @examples
+#' 
+#' # Loading required libraries
+#' library(survival)
+#' library(sandwich)
+#' 
+#' # Reading in a dataset
+#' mri <- read.table("http://www.emersonstatistics.com/datasets/mri.txt",header=TRUE)
+#' attach(mri)
+#' 
+#' # Linear regression of LDL on age (with robust SE by default)
+#' testReg <- regress ("mean", ldl~age)
+#' 
+#' # 95% Prediction Interval for age 50
+#' predict(testReg)
+#' 
+#' @export predict.uRegress
+predict.uRegress <- function(object, interval="prediction",level=0.95,...){
+  
+  ## if reg is not a uRegress object, throw an error
+  if(!("uRegress" %in% class(object))){
+    stop("uRegress object must be entered")
+  }
+  if(class(object$fit)=="lm"){
+    ret <- predict.lm(object$fit,interval=interval,level=level, ...)
+  } else if(class(object$fit)[1]=="glm"){
+    ret <- predict.glm(object$fit,interval=interval,level=level,...)
+  } else if (class(object$fit)[1]=="coxph"){
+    ret <- predict(object$fit,interval=interval,level=level, ...)
+  } else {
+    stop("Predictions not yet implemented for objects of type 'geeglm'")
+  }
+  return(ret)
+}
+
