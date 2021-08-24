86064cfb74522abeb8548b4b53d8b9b70f125cae
Charles Wolock <cwolock@gmail.com>
2021-08-24 13:58:46 -0700

    added fev and psa datasets

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' MRI dataset
 #' 
-#' Data from an observational study of observe the incidence of cardiovascular disease 
+#' Data from an observational study of the incidence of cardiovascular disease 
 #' (especially heart attacks and congestive heart failure) and cerebrovascular disease 
 #' (especially strokes) in the U.S. elderly. More information, including a coding key,
 #'  is available at
@@ -25,3 +25,30 @@
 #' 
 #' @source \url{http://www.emersonstatistics.com/datasets/salary.txt}
 "salary"
+
+#' PSA dataset
+#' 
+#' Data from a study of 50 men having hormonally treated prostate cancer. Includes information on 
+#' PSA levels, tumor characteristics, remission status, age, and disease state.
+#' More information, including a coding key, is available at
+#' \url{http://www.emersonstatistics.com/datasets/PSA.doc}.
+#' 
+#'
+#' @format A data frame with 50 rows and 9 variables.
+#' 
+#' @source \url{http://www.emersonstatistics.com/datasets/psa.txt}
+"psa"
+
+#' FEV dataset
+#' 
+#' Data from a study of 654 children on the relationship between smoking status and 
+#' lung function (measured by FEV). Each row corresponds to a single clinic visit and contains
+#' information on age, height, sex, FEV, and smoking status.
+#' More information, including a coding key, is available at
+#' \url{http://www.emersonstatistics.com/datasets/fev.doc}.
+#' 
+#'
+#' @format A data frame with 654 rows and 7 variables.
+#' 
+#' @source \url{http://www.emersonstatistics.com/datasets/fev.txt}
+"fev"
