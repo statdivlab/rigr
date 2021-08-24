86c8215860ab8abcfe5e692228f886898903c57a
Charles Wolock <cwolock@gmail.com>
2021-08-24 13:31:25 -0700

    added salary dataset and added lazy loading to DESCRIPTION

--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
-#' MRI Dataset
+#' MRI dataset
 #' 
 #' Data from an observational study of observe the incidence of cardiovascular disease 
 #' (especially heart attacks and congestive heart failure) and cerebrovascular disease 
 #' (especially strokes) in the U.S. elderly. More information, including a coding key,
 #'  is available at
-#' \url{http://www.emersonstatistics.com/datasets/mri.pdf}.
+#' \url{http://www.emersonstatistics.com/datasets/mri.doc}.
 #' 
 #'
 #' @format A data frame with 735 rows and 30 variables.
@@ -12,3 +12,16 @@
 #' @source \url{http://www.emersonstatistics.com/datasets/mri.pdf}
 "mri"
 
+#' Salary dataset
+#' 
+#' Data from a study of 1,597 faculty members at a single US university. Includes information on 
+#' monthly salary each year from 1976 through 1995,  as well as sex, highest degree attained, 
+#' year of highest degree, field, year hired, rank, and administrative duties. 
+#' More information, including a coding key, is available at
+#' \url{http://www.emersonstatistics.com/datasets/salary.doc}.
+#' 
+#'
+#' @format A data frame with 19792 rows and 11 variables.
+#' 
+#' @source \url{http://www.emersonstatistics.com/datasets/salary.txt}
+"salary"
