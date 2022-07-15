cde44be111d4450b87ebaa9406674e4681447096
yiqunchen <yiqunchen1995@gmail.com>
2022-07-15 11:01:17 -0700

    change the logic for identifying columns

--- R/regress_utils.R
+++ R/regress_utils.R
@@ -1204,14 +1204,19 @@ processTerm <- function (z, Term, TermName) {
       TermName_split <- unlist(strsplit(TermName,":"))
       
       if (length(TermName_split) == 1) {
-        predNms <- gsub(TermName, "", dimnames(Term)[[2]])
+        # this causes problem if other entries of the dimnames(Term)[[2]] contains 
+        # TermName as well
+        predNms <- gsub(paste0("^",TermName), "", dimnames(Term)[[2]])
       } else {
         dimnames_Term_split <- strsplit(dimnames(Term)[[2]], ":")
         
         for (i in 1:length(TermName_split)) {
           # if the term name is exactly equal to the dimname, don't replace it
           if (TermName_split[i] != dimnames_Term_split[[1]][i]) {
-            dimnames_Term_split <- lapply(dimnames_Term_split, function(x) {c(gsub(TermName_split[i], "", x[i]), x[-i])})
+            #cat("TermName2",dimnames_Term_split,"\n")
+            dimnames_Term_split <- lapply(dimnames_Term_split, 
+                                          function(x) {c(gsub(paste0("^",TermName_split[i]), "", x[i]), x[-i])})
+            
           }
           
         }
@@ -1238,7 +1243,7 @@ processTerm <- function (z, Term, TermName) {
     z$preds <- c(z$preds,TermName)
   }
   z$lastPred[nTerm] <- dim(z$X)[2]
-  z
+  return(z) 
 }
 
 
